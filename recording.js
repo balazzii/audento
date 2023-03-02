36ec65c94f0c56080f347d3534c072b071570182
balazzii <balajiv92@gmail.com>
2023-03-02 21:29:08 +0900

    Added script and footer

--- recording.js
+++ recording.js
@@ -1,11 +1,24 @@
 // set up basic variables for app
+let audioFilesMap = new Map();
+let finalAudioBufferSourceNodes = [];
+let count = 0;
+let finalAudioBufferSourceNodeEndedCount = 0;
+let audioCtx;
+let totalSecs = 100;
+let recordedBuffer;
 
+// Fetch elements for load set up
 const record = document.getElementById("record");
 const stopRec = document.getElementById("stopRec");
 const canvas = document.getElementById("visualizer");
 const canvasFinalLine = document.createElement('canvas');
 let finalLineDiv = document.getElementById("finalLineDiv");
 let finalLineLabel = document.getElementById("finalLineLabel");
+let playRecording = document.getElementById("playRec");
+let mixRecording = document.getElementById("mixRec");
+let stopPlayRecording = document.getElementById("stopPlayRec");
+
+// final audio x-axis set up
 canvasFinalLine.width = finalLineDiv.offsetWidth;
 canvasFinalLine.height = 50;
 canvasFinalLine.setAttribute('width', finalLineDiv.offsetWidth - 15);
@@ -15,15 +28,7 @@ canvasFinalLine.hidden = true;
 finalLineLabel.hidden = true;
 finalLineDiv.appendChild(canvasFinalLine);
 
-let audioFilesMap = new Map();
-let finalAudioBufferSourceNodes = [];
-let count = 0;
-let finalAudioBufferSourceNodeEndedCount = 0;
-
 // visualiser setup - create web audio api context and canvas
-
-let audioCtx;
-let totalSecs = 100;
 const canvasCtx = canvas.getContext("2d");
 const finalLineCtx = canvasFinalLine.getContext("2d");
 
@@ -99,85 +104,15 @@ if (navigator.mediaDevices.getUserMedia) {
     }
 
     mediaRecorder.onstop = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
-      const audio = document.createElement('audio');
-      const divRecContainer = document.getElementById("recContainer");
-      const divActionContainer = document.getElementById("actionContainer");
-
-      const audioRecDiv = document.createElement('div');
-      audioRecDiv.classList.add("col");
-      audioRecDiv.appendChild(audio);
-      divRecContainer.appendChild(audioRecDiv);
-
-      audio.controls = true;
       const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
       chunks = [];
-      const audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      console.log("recorder stopped");
-      
-      // add button
-      const addRecDiv = document.createElement('div');
-      addRecDiv.classList.add("col");
-      addRecDiv.classList.add("col-lg-2");
-      addRecDiv.classList.add("col-md-4");
-      addRecDiv.classList.add("col-sm-8");
-      const addRec = document.createElement('button');
-      addRec.classList.add('btn')
-      addRec.classList.add('btn-info')
-      const addRecIcon = document.createElement('i');
-      addRecIcon.classList.add('bi');
-      addRecIcon.classList.add('bi-plus');
-      addRecIcon.classList.add('btnAddRec');
-      addRec.appendChild(addRecIcon);
-      addRecDiv.appendChild(addRec);
-      
-      // delete button
-      const delRec = document.createElement('button');
-      delRec.classList.add('btn')
-      delRec.classList.add('btn-dark')
-      // delRec.innerHTML = 'Trash';
-      const delRecIcon = document.createElement('i');
-      delRecIcon.classList.add('bi');
-      delRecIcon.classList.add('bi-trash');
-      delRecIcon.classList.add('btnTrash');
-      delRec.appendChild(delRecIcon);
-      addRecDiv.appendChild(delRec);
-      
-      divActionContainer.appendChild(addRecDiv);
 
-
-
-      delRec.onclick = function() {
-        while (divRecContainer.lastElementChild) {
-            divRecContainer.removeChild(divRecContainer.lastElementChild);
-          }
-          while (divActionContainer.lastElementChild) {
-            divActionContainer.removeChild(divActionContainer.lastElementChild);
-          }
-      }
-
-      addRec.onclick = function() {
-        blob.arrayBuffer().then(arrayBuffer => 
-          audioCtx.decodeAudioData(arrayBuffer, (audioBuffer) => {
-            // Do something with audioBuffer
-            console.log(audioBuffer);
-            let key = "finalElement" + count;
-            let gainNode = audioCtx.createGain();
-            audioFilesMap.set(key, {
-              name: "Recording",
-              buffer: audioBuffer,
-              secDelay: 0,
-              xPosition: 0,
-              playOffset: 0,
-              playDuration: audioBuffer.duration,
-              gainNode: gainNode,
-            });
-            gainNode.gain.value = 1;
-            addToFinalAudio("Recording", audioBuffer,key,gainNode);
-            count++;
-          }));
-      }
+      blob.arrayBuffer().then(arrayBuffer => 
+        audioCtx.decodeAudioData(arrayBuffer, (audioBuffer) => {
+          recordedBuffer = audioBuffer;
+          playRecording.disabled = false;
+          mixRecording.disabled = false;
+      }));
     }
 
     mediaRecorder.ondataavailable = function(e) {
@@ -189,8 +124,10 @@ if (navigator.mediaDevices.getUserMedia) {
     console.log('The following error occured: ' + err);
     let warn = document.getElementById("warningLabel");
     warn.hidden = false;
-    record.disabled = true;
-    stopRec.disabled = true;
+    record.hidden = true;
+    stopRec.hidden = true;
+    playRecording.hidden = true;
+    mixRecording.hidden = true;
   }
 
   navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
@@ -263,45 +200,69 @@ function onLoad(){
   "audento-mixer/sounds/door.mp3",
   "audento-mixer/sounds/landslide.mp3",
   "audento-mixer/sounds/rain.mp3",
-  "audento-mixer/sounds/glass-falls.mp3",
+  "audento-mixer/sounds/glass.mp3",
   "audento-mixer/sounds/yodel.mp3",
   "audento-mixer/sounds/piano.mp3",
-  "audento-mixer/sounds/cinematic.mp3",
+  "audento-mixer/sounds/cinema.mp3",
   "audento-mixer/sounds/birds.mp3"
   ]
 
   // load audio files
   sounds.forEach(createPreloadedContainers);
+
+  // resize iFrame
+  let iFrame = document.getElementById("pdfViewer");
+  let audiosContainer = document.getElementById("audiosContainer");
+  let scriptHeader = document.getElementById("scriptHeader");
+  let scriptUploadButton = document.getElementById("scriptUploadButton");
+  iFrame.height = audiosContainer.clientHeight - scriptHeader.clientHeight - scriptUploadButton.clientHeight - 80;
+  document.getElementById('scriptFile').addEventListener('change', function(e) {
+    if (e.target.files[0]) {
+      console.log(e.target.files[0]);
+      alert("Local file needs to be uploaded in server and server functionality is pending.");
+    }
+  });
 }
+
 function createPreloadedContainers(item) {
   const preLoadedContainer = document.getElementById("preLoadedRow");
   
   const div = document.createElement('div');
   div.classList.add('col');
-  div.classList.add('col-sm-6');
-  div.classList.add('col-md-4');
-  div.classList.add('col-lg-2');
+  div.classList.add('col-sm-12');
+  div.classList.add('col-md-6');
+  div.classList.add('col-lg-4');
   div.classList.add('preloadedContainers');
 
-  const h6 = document.createElement('span');
-  h6.classList.add('card-subtitle');
-  h6.classList.add('mb-2');
-  h6.classList.add('text-muted');
+  const preLoadedAudioLabel = document.createElement('span');
+  preLoadedAudioLabel.classList.add('card-subtitle');
+  preLoadedAudioLabel.classList.add('mb-2');
+  preLoadedAudioLabel.classList.add('text-muted');
   var text = item.substring(21, item.length - 4);
-  h6.innerHTML = text;
+  preLoadedAudioLabel.innerHTML = text;
 
   const button = document.createElement("button");
   button.classList.add('btn');
-  button.classList.add('btn-secondary');
+  button.classList.add('btn-outline-success');
   
   const icon = document.createElement('i');
   icon.classList.add("bi");
   icon.classList.add("bi-volume-up");
   button.appendChild(icon);
 
+  const stopbutton = document.createElement("button");
+  stopbutton.classList.add('btn');
+  stopbutton.classList.add('btn-outline-danger');
+  
+  const stopbuttonIcon = document.createElement('i');
+  stopbuttonIcon.classList.add("bi");
+  stopbuttonIcon.classList.add("bi-stop");
+  stopbutton.appendChild(stopbuttonIcon);
+  stopbutton.hidden = true;
+
   const mixButton = document.createElement('button');
-  mixButton.classList.add('btn')
-  mixButton.classList.add('btn-info')
+  mixButton.classList.add('btn');
+  mixButton.classList.add('btn-outline-dark');
   // mixButton.innerHTML = 'Mix';
   const mixButtonIcon = document.createElement('i');
   mixButtonIcon.classList.add('bi');
@@ -310,41 +271,41 @@ function createPreloadedContainers(item) {
   mixButton.appendChild(mixButtonIcon);
 
   div.appendChild(button);
+  div.appendChild(stopbutton);
   div.appendChild(mixButton);
-  div.appendChild(h6);
+  div.appendChild(preLoadedAudioLabel);
 
   preLoadedContainer.appendChild(div);
 
+  let preLoadedSource;
   button.onclick = function() {
+    stopbutton.hidden = false;
+    button.hidden = true;
     let audioSource = null;
-    if(button.classList.contains("btn-secondary")){
-      button.classList.remove("btn-secondary");
-      button.classList.add("btn-primary");
-      if(!audioCtx) {
-        audioCtx = new AudioContext();
-      }
-      window.fetch(item)
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer))
-      .then(audioBuffer => {
-        const source = audioCtx.createBufferSource();
-        source.buffer = audioBuffer;
-        source.connect(audioCtx.destination);
-        source.start();
-        source.addEventListener("ended", () => {
-          button.classList.remove("btn-primary");
-          button.classList.add("btn-secondary");
-        });
-        audioSource = source;
-      });
+    if(!audioCtx) {
+      audioCtx = new AudioContext();
     }
-    else {
-      button.classList.remove("btn-primary");
-      button.classList.add("btn-secondary");
-      if(audioSource) {
-        audioSource.stop();
-      }
-    }
+    window.fetch(item)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      const source = audioCtx.createBufferSource();
+      preLoadedSource = source;
+      source.buffer = audioBuffer;
+      source.connect(audioCtx.destination);
+      source.start();
+      source.addEventListener("ended", () => {
+        stopbutton.hidden = true;
+        button.hidden = false;
+    });
+    audioSource = source;
+    });
+  }
+
+  stopbutton.onclick = () => {
+    button.hidden = false;
+    stopbutton.hidden = true;
+    preLoadedSource.stop();
   }
 
   mixButton.onclick = function() {
@@ -629,4 +590,53 @@ function stopFinal() {
   finalAudioBufferSourceNodes = [];
   changeToPlayButton();
   stopButton.disabled = true;
+}
+
+mixRecording.onclick = () => {
+  // Do something with audioBuffer
+  let key = "finalElement" + count;
+  let gainNode = audioCtx.createGain();
+  audioFilesMap.set(key, {
+    name: "Recording",
+    buffer: recordedBuffer,
+    secDelay: 0,
+    xPosition: 0,
+    playOffset: 0,
+    playDuration: recordedBuffer.duration,
+    gainNode: gainNode,
+  });
+  gainNode.gain.value = 1;
+  addToFinalAudio("Recording", recordedBuffer,key,gainNode);
+  count++;
+}
+
+let recordingTrackSource;
+playRecording.onclick = ()=>{
+  playRecording.hidden = true;
+  stopPlayRecording.hidden = false;
+  
+  if(!audioCtx) {
+    audioCtx = new AudioContext();
+  }
+
+  if (audioCtx.state === "suspended") {
+    audioCtx.resume();
+  }
+
+  const trackSource = new AudioBufferSourceNode(audioCtx, {
+    buffer: recordedBuffer,
+  });
+  recordingTrackSource = trackSource;
+  trackSource.connect(audioCtx.destination);
+  trackSource.start();
+  trackSource.addEventListener("ended", () => {
+    playRecording.hidden = false;
+    stopPlayRecording.hidden = true;
+  });
+}
+
+stopPlayRecording.onclick = ()=>{
+  playRecording.hidden = false;
+  stopPlayRecording.hidden = true;
+  recordingTrackSource.stop();
 }
